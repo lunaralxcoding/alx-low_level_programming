b9e935076be4e1400897646f12c5ad544d553657
lunaralxcoding <developercoding9@gmail.com>
2023-06-17 16:47:09 -0700

    r

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>/
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - entry point 
  *
- * Return: 0 for successful execution
+ * Description: positive or negative
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-
-	return (0);
-}
+	 if (n > 0)
+		 printf("%d is positive\n", n);
+	 else if (n == 0)
+		 printf("%d is zero\n", n);
+	 else
+		  printf("%d is negative\n", n);
+	 return (0);
+}	  
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
