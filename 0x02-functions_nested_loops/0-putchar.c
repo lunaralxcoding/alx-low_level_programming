95a5ba985b652a8eeb4601514bff65297093bcf4
lunaralxcoding <developercoding9@gmail.com>
2023-06-21 06:49:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: prints putchar using putchar prototype
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+	return (0);
 }
