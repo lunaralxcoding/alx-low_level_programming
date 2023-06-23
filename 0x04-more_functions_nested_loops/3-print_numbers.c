3409945e97c930d190568c8556ba4b8c524533bd
lunaralxcoding <developercoding9@gmail.com>
2023-06-22 19:20:29 -0700

    g

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - print 0-9
  * return: void
@@ -11,8 +12,6 @@ void print_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
-
 	}
 	_putchar('\n');
-
 }
